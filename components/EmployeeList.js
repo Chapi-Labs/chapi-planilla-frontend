d4185ca59205da00dbf98a99065a24b878534c55
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-04-14 13:29:52 -0600

    add editable item data table

--- components/EmployeeList.js
+++ components/EmployeeList.js
@@ -1,88 +1,104 @@
-import React, { Component } from 'react';
-import Router from 'next/router';
-import Link from 'next/link';
-import { Mutation } from 'react-apollo';
-import Nav from '../components/Nav';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import { MDBDataTable } from 'mdbreact';
-import Editable from 'react-x-editable';
-import gql from 'graphql-tag';
+import React, { Component } from "react";
+import Link from "next/link";
+import { Query, Mutation } from "react-apollo";
+import { MDBDataTable } from "mdbreact";
+import gql from "graphql-tag";
+import Editable from "react-x-editable";
+import Nav from "../components/Nav";
 
-const EMPLOYEE_MUTATION = gql`
-  mutation EMPLOYEE_MUTATION(
-    $first_name: String!
-    $last_name: String!
-    $email: String!
+const LIST_EMPLOYEE = gql`
+  query LIST_EMPLOYEE {
+    employees {
+      id
+      first_name
+      last_name
+      email
+    }
+  }
+`;
+const UPDATE_EMPLOYEE_MUTATION = gql`
+  mutation UPDATE_EMPLOYEE_MUTATION(
+    $id: ID!
+    $first_name: String
+    $last_name: String
+    $email: String
   ) {
-    createEmployee(
+    updateEmployee(
+      id: $id
       first_name: $first_name
       last_name: $last_name
       email: $email
     ) {
       id
+      first_name
+      last_name
       email
     }
   }
 `;
 const data = {
-    columns: [
-        {
-            label: 'Name',
-            field: 'name',
-            sort: 'asc',
-            width: 150
-        },
-        {
-            label: 'Position',
-            field: 'position',
-            sort: 'asc',
-            width: 270
-        },
-        {
-            label: 'Office',
-            field: 'office',
-            sort: 'asc',
-            width: 200
-        },
-        {
-            label: 'Age',
-            field: 'age',
-            sort: 'asc',
-            width: 100
-        },
-        {
-            label: 'Start date',
-            field: 'date',
-            sort: 'asc',
-            width: 150
-        },
-        {
-            label: 'Salary',
-            field: 'salary',
-            sort: 'asc',
-            width: 100
-        }
-    ],
-    rows: [
-        {
-            name: <Editable dataType="text" mode="inline" value="LOL" />,
-            position: 'Senior Javascript Developer',
-            office: 'Edinburgh',
-            age: '22',
-            date: '2012/03/29',
-            salary: <Editable dataType="text" mode="inline" value="d" />
-        }
-    ]
+  columns: [
+    {
+      label: "Nombres",
+      field: "first_name",
+      sort: "asc",
+      width: 150
+    },
+    {
+      label: "Apellidos",
+      field: "last_name",
+      sort: "asc",
+      width: 270
+    },
+    {
+      label: "Email",
+      field: "email",
+      sort: "asc",
+      width: 200
+    }
+  ],
+  rows: []
 };
 class EmployeeList extends Component {
-  state = {
-    first_name: '',
-    last_name: '',
-    email: ''
+  handleSubmit(target) {
+    target.props.updateFunction({
+      variables: {
+        id: target.props.id,
+        [target.props.column]: target.newValue
+      }
+    });
+  }
+  transformData = apiData => {
+    const rows = [];
+    for (const row of apiData.employees) {
+      let newRow = {};
+      for (const key in data.columns) {
+        const column = data.columns[key];
+        newRow = {
+          ...newRow,
+          [column.field]: (
+            <Mutation mutation={UPDATE_EMPLOYEE_MUTATION}>
+              {(updateEmployee, { loading, error }) => (
+                <Editable
+                  dataType="text"
+                  mode="inline"
+                  value={row[column.field]}
+                  id={row.id}
+                  column={column.field}
+                  updateFunction={updateEmployee}
+                  handleSubmit={this.handleSubmit}
+                />
+              )}
+            </Mutation>
+          )
+        };
+      }
+      rows.push(newRow);
+    }
+
+    return { ...data, rows };
   };
-  saveToState = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+
   render() {
     return (
       <div>
@@ -105,25 +121,27 @@ class EmployeeList extends Component {
               </div>
             </div>
             <div className="row">
-                <div className="col-12">
-                    <div className="card">
-                        <div className="card-body">
-
-                            <h4 className="mt-0 header-title">Default Datatable</h4>
-                            <p className="text-muted m-b-30">DataTables has most features enabled by
-                                default, so all you need to do to use it with your own tables is to call
-                            the construction function: <code>$().DataTable();</code>.
-                        </p>
-
-                            <MDBDataTable
-                                bordered
-                                hover
-                                search={"props.value"}
-                                data={data}
-                            />
-                        </div>
-                    </div>
+              <div className="col-12">
+                <div className="card">
+                  <div className="card-body">
+                    <h4 className="mt-0 header-title">
+                      Listado de Colaboradores
+                    </h4>
+                    <Query query={LIST_EMPLOYEE}>
+                      {({ error, loading, data }) => {
+                        if (loading) return <p>Loading..</p>;
+                        return (
+                          <MDBDataTable
+                            bordered
+                            hover
+                            data={this.transformData(data)}
+                          />
+                        );
+                      }}
+                    </Query>
+                  </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
