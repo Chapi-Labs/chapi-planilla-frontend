886d3cf790c908ca50db7fd5570f10b5911a1e9a
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-04-14 16:30:39 -0600

    add error and loading

---
+++ components/Error.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const DisplayError = ({ error }) => {
+  if (!error || !error.message) return null;
+  if (error.networkError && error.networkError.result && error.networkError.result.errors.length) {
+    return error.networkError.result.errors.map((error, i) => (
+    <div className="alert alert-danger mb-0" role="alert"> key={i}>
+        <p data-test="graphql-error">
+          <strong>Error!</strong>
+          {error.message.replace('GraphQL error: ', '')}
+        </p>
+      </div>
+    ));
+  }
+  return (
+    <div className="btn btn-outline-danger btn-block m-b-10">
+      <span data-test="graphql-error">
+        <strong>Error! </strong>
+        {error.message.replace("GraphQL error: ", "")}
+      </span>
+    </div>
+  );
+};
+
+DisplayError.defaultProps = {
+  error: {},
+};
+
+DisplayError.propTypes = {
+  error: PropTypes.object,
+};
+
+export default DisplayError;
--- components/Login.js
+++ components/Login.js
@@ -4,7 +4,8 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-
+import Loading from './Loading';
+import Error from './Error';
 
 const SIGNIN_MUTATION = gql`
   mutation SIGNIN_MUTATION($email: String!, $password: String!) {
@@ -27,92 +28,98 @@ class Login extends Component {
   };
   render() {
     return (
-      <Mutation
-        mutation={SIGNIN_MUTATION}
-        variables={this.state}
-        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-      >
-        {(signup, { error, loading }) => (
-          <div className="wrapper-page">
-            <div className="card overflow-hidden account-card mx-3">
-              <div className="bg-primary p-4 text-white text-center position-relative">
-                <h4 className="font-20 m-b-5">Bienvenido</h4>
-                <p className="text-white-50 mb-4">Chapi Planilla</p>
-                <div className="logo logo-admin">
-                  <Link href="/">
-                    <img
-                      src="../static/assets/images/logo-sm.png"
-                      height="24"
-                      alt="logo"
-                    />
-                  </Link>
-                </div>
-              </div>
-              <div className="account-card-content">
-                <Form
-                  method="post"
-                  className="form-horizontal m-t-30"
-                  onSubmit={async e => {
-                    e.preventDefault();
-                    await signup();
-                    this.setState({ name: '', email: '', password: '' });
-                  }}
-                >
-                  <FormGroup>
-                    <Label htmlFor="username">Email</Label>
-                    <Input
-                      type="email"
-                      name="email"
-                      className="form-control"
-                      placeholder="Ingresar email"
-                      value={this.state.email}
-                      onChange={this.saveToState}
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <Label htmlFor="userpassword">Contraseña</Label>
-                    <Input
-                      type="password"
-                      name="password"
-                      className="form-control"
-                      placeholder="Ingresar contraseña"
-                      value={this.state.password}
-                      onChange={this.saveToState}
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <div className="col-sm-3 offset-sm-4">
-                      <Button
-                        className="btn btn-primary w-md waves-effect waves-light"
-                        type="submit"
-                      >
-                        Ingresar
-                      </Button>
-                    </div>
-                  </FormGroup>
-
-                  <FormGroup>
-                    <div className="col-12 m-t-20">
-                      <Link href="/">
-                        <a>
-                          <i className="mdi mdi-lock" /> Recuperar
-                          contraseña
-                        </a>
-                      </Link>
-                    </div>
-                  </FormGroup>
-                </Form>
-              </div>
-            </div>
-
-            <div className="m-t-40 text-center">
-              <p>© {new Date().getFullYear()} Chapi Labs </p>
+      <div className="wrapper-page">
+        <div className="card overflow-hidden account-card mx-3">
+          <div className="bg-primary p-4 text-white text-center position-relative">
+            <h4 className="font-20 m-b-5">Bienvenido</h4>
+            <p className="text-white-50 mb-4">Chapi Planilla</p>
+            <div className="logo logo-admin">
+              <Link href="/">
+                <img
+                  src="../static/assets/images/logo-sm.png"
+                  height="50"
+                  alt="logo"
+                />
+              </Link>
             </div>
           </div>
-        )}
-      </Mutation>
+          <div className="account-card-content">
+            <Mutation
+              mutation={SIGNIN_MUTATION}
+              variables={this.state}
+              refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+            >
+              {(signup, { error, loading }) => {
+                if (loading) return <Loading loading={loading}/>
+                return (
+                  <Form
+                    method="post"
+                    className="form-horizontal m-t-30"
+                    onSubmit={async e => {
+                      e.preventDefault();
+                      await signup();
+                      this.setState({ name: '', email: '', password: '' });
+                    }}
+                  >
+                    <Error error={error} />
+                    <FormGroup>
+                      <Label htmlFor="username">Email</Label>
+                      <Input
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Ingresar email"
+                        autoComplete="email"
+                        value={this.state.email}
+                        onChange={this.saveToState}
+                      />
+                    </FormGroup>
+
+                    <FormGroup>
+                      <Label htmlFor="userpassword">Contraseña</Label>
+                      <Input
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        autoComplete="password"
+                        placeholder="Ingresar contraseña"
+                        value={this.state.password}
+                        onChange={this.saveToState}
+                      />
+                    </FormGroup>
+
+                    <FormGroup>
+                      <div className="col-sm-3 offset-sm-4">
+                        <Button
+                          className="btn btn-primary w-md waves-effect waves-light"
+                          type="submit"
+                        >
+                          Ingresar
+                        </Button>
+                      </div>
+                    </FormGroup>
+
+                    <FormGroup>
+                      <div className="col-12 m-t-20">
+                        <Link href="/">
+                          <a>
+                            <i className="mdi mdi-lock" /> Recuperar
+                            contraseña
+                          </a>
+                        </Link>
+                      </div>
+                    </FormGroup>
+                  </Form>
+                )
+              }}
+            </Mutation>
+        </div>
+      </div>
+
+      <div className="m-t-40 text-center">
+        <p>© {new Date().getFullYear()} Chapi Labs </p>
+      </div>
+    </div>
     );
   }
 }
