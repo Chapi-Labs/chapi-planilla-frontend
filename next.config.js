9f63f1c52a541af552646f42ffb049da44520f5b
Pablo Díaz <pablo5_diaz@hotmail.com>
2019-04-15 17:26:49 -0600

    add dotenv

--- next.config.js
+++ next.config.js
@@ -2,31 +2,33 @@ const withSass = require('@zeit/next-sass');
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 const withFonts = require('next-fonts');
+const dotEnvResult = require("dotenv").config();
 
-module.exports = withPlugins(
-  [
-    withCSS,
-    withSass,
-  ],
-  {
-    webpack: config => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty'
-      }
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000000,
-            name: '[name].[ext]'
-          }
+
+if (dotEnvResult.error) {
+  throw dotEnvResult.error;
+}
+
+module.exports = withPlugins([withCSS, withSass], {
+  webpack: config => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: "empty"
+    };
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 10000000,
+          name: "[name].[ext]"
         }
-      })
+      }
+    });
 
-      return config
-    },
-    target: 'serverless'
+    return config;
+  },
+  env: {
+    ...dotEnvResult
   }
-);
+});
